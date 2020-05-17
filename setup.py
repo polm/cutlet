7cd38a6fc5ecedcdd27a44e0720ef4cae162f84d
Paul O'Leary McCann <polm@dampfkraft.com>
2020-05-17 15:03:11 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import pathlib
 
 setup(name='cutlet', 
-      version='0.1.2',
+      version='0.1.3',
       author="Paul O'Leary McCann",
       author_email="polm@dampfkraft.com",
       description="Romaji converter",
