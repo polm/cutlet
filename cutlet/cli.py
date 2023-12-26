46d6550500461d0c6131dfba6bcdba3bb58b7c64
Hizuru <106918920+Hizuru3@users.noreply.github.com>
2023-12-26 17:28:38 +0900

    Make compatible with Windows OS

--- cutlet/cli.py
+++ cutlet/cli.py
@@ -2,12 +2,15 @@ from cutlet import Cutlet
 import fileinput
 import sys
 
-# Don't print an error on SIGPIPE
-from signal import signal, SIGPIPE, SIG_DFL
 
 def main():
-    signal(SIGPIPE, SIG_DFL) 
-    system = sys.argv[1] if len(sys.argv) > 1 else 'hepburn'
+    if sys.platform != "win32":
+        # Don't print an error on SIGPIPE
+        from signal import signal, SIGPIPE, SIG_DFL
+
+        signal(SIGPIPE, SIG_DFL)
+
+    system = sys.argv[1] if len(sys.argv) > 1 else "hepburn"
 
     katsu = Cutlet(system)
 
